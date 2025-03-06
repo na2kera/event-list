9a328a3652686de3fd55883198747b391c1223b4
na2kera <nat3mitaro@gmail.com>
2025-03-06 16:32:38 +0900

    delete: 不要な部分を削除

--- app/auth/signin/page.tsx
+++ app/auth/signin/page.tsx
@@ -1,11 +1,9 @@
 "use client";
 
 import { signIn } from "next-auth/react";
-import { useRouter } from "next/navigation";
 import { useState } from "react";
 
 export default function SignIn() {
-  const router = useRouter();
   const [error, setError] = useState<string | null>(null);
 
   const handleSignIn = async () => {
